ca8d7d10c37ee7b474b80ef5a0ede56ca51d73f0
Joseph Morales Silva <josephzx1@hotmail.com>
2023-12-31 10:36:54 -0600

    Refactor input elements and add lifecycle hooks to CounterComponent

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -21,4 +21,26 @@ export class CounterComponent {
     console.log('onChanges', changes);
     console.log('-'.repeat(10));
   }
+  ngOnInit() {
+    // se ejecuta una sola vez después de ngOnChanges
+    // es donde podemos ejecutar todo el código asíncrono que necesitemos
+    console.log('OnInit method');
+    console.log('-'.repeat(10));
+  }
+  ngAfterViewInit() {
+    /**
+     * se inicia una vez los componentes hijos están listos
+     */
+    console.log('ngAfterViewInit method');
+    console.log('-'.repeat(10));
+  }
+  ngOnDestroy() {
+    /**
+     * Se ejecuta cuando se desmonta el componente,
+     * se usa generalmente para eliminar suscripciones
+     *  o detener eventos asíncronos
+     */
+    console.log('ngOnDestroy method');
+    console.log('-'.repeat(10));
+  }
 }
