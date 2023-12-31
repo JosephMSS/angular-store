41cb09dd34654e25a912337bc4d9c3bc9cb6ce23
Joseph Morales Silva <josephzx1@hotmail.com>
2023-12-31 09:45:02 -0600

    Add about component with counter

---
+++ src/app/domains/info/pages/about/about.component.ts
@@ -0,0 +1,18 @@
+import { Component, signal } from '@angular/core';
+import { CounterComponent } from '../../../shared/components/counter/counter.component';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [CounterComponent],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.css',
+})
+export class AboutComponent {
+  duration = signal(1000);
+  message = signal('message');
+  changeDurationHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.duration.update((_state) => input.valueAsNumber);
+  }
+}
